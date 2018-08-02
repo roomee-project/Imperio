f2cf01c27d851b7c5c9c2a5c3ea2f7cee74f30c7
Kalei Trujillo <kalei096@gmail.com>
2018-08-02 15:00:11 -0700

    footer

--- client/src/components/routes/index.jsx
+++ client/src/components/routes/index.jsx
@@ -32,7 +32,11 @@ const Routes = (props) => (
       <Route path="/logout" exact component={MapContainer} />
       <Route path="/zipform" exact component={ZipForm} />
     </Switch>
-
+    <footer className="container-fluid p-3 mb-2 bg-light text-dark">
+    <div class="row align-items-end">
+    <h6 className="">the roomee project edition</h6>
+    </div>
+  </footer>
     </div>
   </Router>
 
